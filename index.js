9e327bcb32c866c0e7ce990728bd3aa482150601
Duong Tran <duongtran@example.com>
2026-03-17 21:41:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const app = express ();
+const PORT = 3000;
+app.listen (PORT, () => {
+    console.log (`Server is running on port ${PORT}`);
+});
+
+const bodyParser = require ('body-parser');
+app.use(bodyParser.json ({linit: '10mb'}));
+app.use(bodyParser.urlencoded ({extended: true, limit: '10mb'}));
+
+app.get ('/', (req, res) => {
+    res.json ({message:'Hello from server !' });
+});
